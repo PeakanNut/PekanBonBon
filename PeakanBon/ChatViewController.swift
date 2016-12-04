c917348d82bbbbf48332f2b307d34532571dc94c
gongzheng <gongzheng@gongzhengdeMacBook-Pro.local>
2016-12-04 16:35:26 +0900

    add play movie

--- PeakanBon/ChatViewController.swift
+++ PeakanBon/ChatViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import JSQMessagesViewController
+import MobileCoreServices
+import AVKit
 
 
 
@@ -20,6 +22,7 @@ class ChatViewController: JSQMessagesViewController {
         
         self.senderId = "1"
         self.senderDisplayName = "zero2launch"
+       
         
 
         // Do any additional setup after loading the view.
@@ -39,6 +42,30 @@ class ChatViewController: JSQMessagesViewController {
     
     override func didPressAccessoryButton(_ sender: UIButton!) {
         print("accessoryButton")
+        let sheet = UIAlertController.init(title: "Media Message", message: "Please select a media", preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        let cancel = UIAlertAction.init(title: "Cancel", style: UIAlertActionStyle.cancel, handler:nil)
+        let photoLibrary = UIAlertAction.init(title: "Photo Library", style: UIAlertActionStyle.default){
+            (alert:UIAlertAction)in
+            self.getMediaFrom(type: kUTTypeImage)
+        }
+        let movieLibrary = UIAlertAction.init(title: "Video Library", style: UIAlertActionStyle.default){
+            (alert:UIAlertAction)in
+            self.getMediaFrom(type: kUTTypeMovie)
+
+        }
+        
+        sheet.addAction(photoLibrary)
+        sheet.addAction(movieLibrary)
+        sheet.addAction(cancel)
+        self.present(sheet, animated: true, completion: nil)
+    }
+    
+    func getMediaFrom(type: CFString){
+        let mediaPicker = UIImagePickerController()
+        mediaPicker.delegate = self
+        mediaPicker.mediaTypes = [type as String]
+        self.present(mediaPicker, animated: true, completion: nil)
     }
     
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
@@ -63,6 +90,18 @@ class ChatViewController: JSQMessagesViewController {
         return cell
     }
     
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, didTapMessageBubbleAt indexPath: IndexPath!) {
+        let message = messages[indexPath.item]
+        if message.isMediaMessage{
+            if let mediaItem = message.media as? JSQVideoMediaItem{
+                let player = AVPlayer(url:mediaItem.fileURL)
+                let playerViewController = AVPlayerViewController()
+                playerViewController.player = player
+                self.present(playerViewController, animated: true, completion: nil)
+            }
+        }
+    }
+    
     @IBAction func logOutDidClicked(_ sender: Any) {
         print("logOut Anoymously did taped")
         
@@ -77,3 +116,24 @@ class ChatViewController: JSQMessagesViewController {
     }
 
 }
+
+
+extension ChatViewController:UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        print("did finish picking")
+        print(info)
+        if let picture = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            let photo = JSQPhotoMediaItem(image:picture)
+            messages.append(JSQMessage(senderId:senderId, displayName:senderDisplayName,media:photo))
+        }
+        else if let video = info[UIImagePickerControllerMediaURL] as? NSURL {
+            let videoItem = JSQVideoMediaItem(fileURL: video as URL!, isReadyToPlay:true)
+            messages.append(JSQMessage(senderId:senderId, displayName:senderDisplayName,media: videoItem))
+        }
+        self.dismiss(animated: true, completion: nil)
+        collectionView.reloadData()
+    }
+}
+
